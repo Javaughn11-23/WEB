ac53738bd076be2e98bcc46877aa10ad3640a095
Javaughn11-23 <jayycharles8@gmail.com>
2025-11-07 16:32:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('enter-text').addEventListener('click', () => {
+  document.getElementById('enter-screen').classList.add('hidden');
+  document.getElementById('main-site').classList.remove('hidden');
+});
